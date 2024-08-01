f2160c9bb9b43ce5d0b719cfb1b19bdd1803998e
Koala <76986040+Koala3353@users.noreply.github.com>
2024-08-01 08:31:36 +0800

    hangman feature

---
+++ src/main/java/com/general_hello/commands/commands/Hangman/VirtualBot.java
@@ -0,0 +1,190 @@
+package com.general_hello.commands.commands.Hangman;
+
+import com.general_hello.commands.Database.DatabaseManager;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import static com.general_hello.commands.commands.Hangman.VirtualBot.state.*;
+
+
+/**
+ * This class represents a virtual bot, communicating through the hangman bot but acting as though it was only
+ * working within a single guild (discord server). Upon construction, a guild is supplied to make sure the virtual bot
+ * can detect if it is handling messages from the wrong guild.
+ */
+public class VirtualBot extends ListenerAdapter {
+
+    /* The bot starts in an idle state. The setup state is used when listening for setup details from
+    the user that used the start command. The playing state is used when a game is being played in a channel. */
+    enum state {IDLE, SETUP, PLAYING}
+
+    public static final String START_COMMAND = "ignthangman-start";
+    public static final String RESET_COMMAND = "ignthangman-reset";
+    private final Guild guild;
+
+    private state currentState;
+    private Game currentGame;
+    private User startingUser;
+    private TextChannel gameChannel;
+
+    public VirtualBot(Guild g) {
+        currentState = IDLE;
+        guild = g;
+    }
+
+    /**
+     * Sends a MessageReceivedEvent to this bot for it to handle. Individual bots do not implement the
+     * ListenerAdapter interface as this would be inefficient.
+     * @param event The MessageReceivedEvent we want this bot to handle.
+     */
+    public void handleMessageEvent(MessageReceivedEvent event) {
+        if (shouldIgnore(event)) return;
+        checkGuild(event);
+
+        // At any time, the bot can be reset, either through DM or text channels.
+        String words = event.getMessage().getContentRaw().replaceAll("\\s++", "");
+        if (words.equals(RESET_COMMAND)) {
+            event.getChannel().sendMessage("I've been reset!").queue();
+            reset();
+            return;
+        }
+
+        switch (currentState) {
+            case IDLE:
+                handleStart(event);
+                break;
+            case SETUP:
+                handleSetup(event);
+                break;
+            case PLAYING:
+                handlePlaying(event);
+                break;
+        }
+
+    }
+
+    // We should ignore events from bots, and messages from channels other than
+    private boolean shouldIgnore(MessageReceivedEvent event) {
+        return ((event.getChannelType() != ChannelType.TEXT ) && (event.getChannelType() != ChannelType.PRIVATE)
+                || event.getAuthor().isBot());
+    }
+
+    // This bot instance has its own guild, and should not handle events from other guilds
+    private void checkGuild(MessageReceivedEvent event) {
+        if(event.getChannelType() == ChannelType.TEXT && event.getGuild() != guild) {
+            throw new IllegalArgumentException(
+                    "Event from guild " + event.getGuild() + " passed to bot for guild " + guild);
+        }
+    }
+
+    // Resets all state of the bot, effectively returning to its starting state after construction
+    private void reset() {
+        currentGame = null;
+        startingUser = null;
+        currentState = IDLE;
+        gameChannel = null;
+    }
+
+    private void handlePlaying(MessageReceivedEvent event) {
+        if(event.getChannel() == gameChannel) {
+            if (event.getAuthor() == startingUser) {
+                gameChannel.sendMessage("No cheating, " + startingUser.getName() + "! :angry:").queue();
+                return;
+            }
+            String message = event.getMessage().getContentRaw().trim().toUpperCase();
+            if (message.length() == 1) {
+                currentGame.guessChar(message.charAt(0));
+                gameChannel.sendMessage("You guessed: " + message.charAt(0)).queue();
+            } else {
+                currentGame.guessWord(message);
+                gameChannel.sendMessage("You guessed: " + message).queue();
+            }
+            gameChannel.sendMessage(currentGame.displayGameState()).queue();
+            if (currentGame.isWon() || currentGame.isLost()) {
+                gameChannel.sendMessage(currentGame.isWon() ? "Game won!" : "Game lost!").queue();
+                if (currentGame.isWon()) {
+                    DatabaseManager.INSTANCE.setCredits(event.getAuthor().getIdLong(), 3000);
+                }
+                reset();
+            }
+        }
+    }
+
+    private void handleStart(MessageReceivedEvent event) {
+        String firstWord = event.getMessage().getContentRaw().toLowerCase().replaceAll("\\s++", "");
+        System.out.println(firstWord);
+        if (!firstWord.equals(START_COMMAND)) return;
+
+        startingUser = event.getAuthor();
+        gameChannel = event.getTextChannel();
+        currentState = SETUP;
+
+        String privateMessage = "Please respond with a single number in the range 1 to " +
+                Game.MAX_ALLOWED_FAILS + " of allowed fails followed by a space and a word or sentence to guess.";
+
+        gameChannel.sendMessage(
+                "Let's go! Send me a private message with the details, " + startingUser.getName() + ".").queue();
+        messageUser(startingUser, privateMessage);
+    }
+
+    private void handleSetup(MessageReceivedEvent event) {
+        if (!(event.getChannelType() == ChannelType.PRIVATE && event.getAuthor() == startingUser)) return;
+
+        boolean success = true;
+
+        String errorMessage = "Failed to read your message. ";
+        String cleanedContent = event.getMessage().getContentRaw()
+                .trim()
+                .toUpperCase()
+                .replaceAll("\\p{Punct}", "")
+                .replaceAll("\\s+", " ");
+        String[] cleanedContentWords = cleanedContent.split(" ");
+
+        int fails = -1;
+        String guessingSentence = "";
+
+        try {
+            if (cleanedContentWords.length < 1) {
+                success = false;
+                errorMessage += "Your message was empty. ";
+            } else {
+                fails = Integer.parseInt(cleanedContentWords[0]);
+                if (!(1 <= fails && fails <= Game.MAX_ALLOWED_FAILS)) {
+                    success = false;
+                    errorMessage += "Start with a number in the range 1 to " + Game.MAX_ALLOWED_FAILS + ". ";
+                }
+            }
+        } catch (NumberFormatException e) {
+            success = false;
+            errorMessage += "Start with a number in the range 1 to " + Game.MAX_ALLOWED_FAILS + ". ";
+        }
+
+        if (cleanedContentWords.length < 2) {
+            success = false;
+            errorMessage += "Failed to identify a guessing sentence or word. ";
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < cleanedContentWords.length; i++) {
+                sb.append(cleanedContentWords[i]).append(i == cleanedContentWords.length - 1 ? "" : " ");
+            }
+            guessingSentence = sb.toString();
+        }
+
+        // If setup successful, start game. Else, remain in the setup state with the same user and inform that user.
+        if(success) {
+            currentGame = new Game(guessingSentence, fails);
+            currentState = PLAYING;
+            gameChannel.sendMessage("Setup complete, game starting...").queue();
+            gameChannel.sendMessage(currentGame.displayGameState()).queue();
+        } else {
+            messageUser(startingUser, errorMessage);
+        }
+
+    }
+
+    // Send a message to a user without response handling
+    private void messageUser(User u, String message) {
+        u.openPrivateChannel().queue(channel -> channel.sendMessage(message).queue());
+    }
+}
---
+++ src/main/java/com/general_hello/commands/commands/Hangman/VirtualBotManager.java
@@ -0,0 +1,107 @@
+package com.general_hello.commands.commands.Hangman;
+
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * The virtual bot manager maintains a bot for each guild of the actual bot and distributes any message events
+ * to the correct virtual bot based on the guild and/or user from which the message was sent. The
+ * VirtualBotManager tries to use all logical threads available on the system. The virtual bot manager handles the
+ * creation of all virtual bots, and the VirtualBot class should not be used if this class is used.
+ */
+public class VirtualBotManager extends ListenerAdapter {
+
+    // All virtual bots are kept in a hash table, ensures at most one bot per guild
+    private final HashMap<Guild, VirtualBot> virtualBots;
+
+    /* Users may choose to use multiple or a single core, whatever works best.
+    All methods in this class have two behaviours; one for multithreading and one for
+    single thread use. The state below is only used if multithreading is true.
+    It would be a good idea to make this class abstract and implement two subclasses; one for
+    single thread use and one for parallel use. */
+    private final boolean multithreading;
+
+    // Each virtual bot has a corresponding executor service, if multithreaded
+    private HashMap<VirtualBot, ExecutorService> botExecutors;
+
+    // We maintain n-1 single thread executors, where n is the number of logical threads.
+    private List<ExecutorService> singleThreadExecutors;
+
+    // Keeps track of what executor to assign to next virtual bot
+    private int execIndex;
+
+
+    /**
+     * Creates a new virtual bot manager. It is not recommended to ever instantiate more than one.
+     * If the multithreading parameter is set to true, the VirtualBotManager will attempt to use one
+     * core per logical thread and split the work between each core.
+     * @param multithreading If true, multiple cores are used. Else, a single thread is used.
+     */
+    public VirtualBotManager(boolean multithreading) {
+        virtualBots = new HashMap<>();
+
+        if(Runtime.getRuntime().availableProcessors() > 1) {
+            this.multithreading = multithreading;
+        } else {
+            this.multithreading = false; // If there is only one logical thread, we shouldnt waste resources
+        }
+
+        if(multithreading) {
+            botExecutors = new HashMap<>();
+            singleThreadExecutors = new ArrayList<>();
+            int cores = Runtime.getRuntime().availableProcessors();
+            for (int i = 0; i < cores - 1; i++) singleThreadExecutors.add(Executors.newSingleThreadExecutor());
+            execIndex = 0;
+        }
+    }
+
+    /**
+     * Receives an event from the event queue and sends it to the appropriate virtual bot. If the message
+     * was a text message, it is sent to the corresponding virtual bot. If the message was a DM,
+     * it is sent to the virtual bots of all guilds to which the sender belongs (all mutual guilds).
+     * @param event The event received from the api.
+     */
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        ChannelType t = event.getChannelType();
+        if (t == ChannelType.TEXT) {
+            VirtualBot b = safeGetBot(event.getGuild());
+            if(multithreading) {
+                botExecutors.get(b).submit(() -> b.handleMessageEvent(event));
+            } else {
+                b.handleMessageEvent(event);
+            }
+        } else if (t == ChannelType.PRIVATE) {
+            for(Guild mutualGuild : event.getAuthor().getMutualGuilds()) {
+                VirtualBot b = safeGetBot(mutualGuild);
+                if(multithreading) {
+                    botExecutors.get(b).submit(() -> b.handleMessageEvent(event));
+                } else {
+                    b.handleMessageEvent(event);
+                }
+            }
+        }
+    }
+
+    // Either sends the bot to the correct virtual bot, or creates a new one if there is no such virtual bot
+    // If multithreading is on and there is no bot, the bot is also assigned to an executor.
+    private VirtualBot safeGetBot(Guild g) {
+        VirtualBot b = virtualBots.get(g);
+        if (b == null) {
+            b = new VirtualBot(g);
+            virtualBots.put(g, b);
+            if(multithreading) {
+                botExecutors.put(b, singleThreadExecutors.get((execIndex++) % singleThreadExecutors.size()));
+            }
+        }
+        return b;
+    }
+}
